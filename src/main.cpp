36cf2957005d080e108571cfeb46aea9e261f7cd
Kakosyan <ed217980@dal.ca>
2024-07-16 12:16:02 -0300

    [New] Colored cube with depth check function

--- src/main.cpp
+++ src/main.cpp
@@ -53,6 +53,10 @@ int main() {
   // Dark blue background
   glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
 
+  // Enable Depth test
+  glEnable(GL_DEPTH_TEST);
+  glDepthFunc(GL_LESS);
+
   GLuint VertexArrayID;
   glGenVertexArrays(1, &VertexArrayID);
   glBindVertexArray(VertexArrayID);
@@ -62,34 +66,111 @@ int main() {
       programID = LoadShaders("../src/SimpleTransform.vertexshader", "../src/SimpleFragmentShader.fragmentshader");
 
   GLuint MatrixID = glGetUniformLocation(programID, "MVP");
-  glm::mat4 Projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.0f);
+  glm::mat4 Projection = glm::perspective(glm::radians(60.0f), 4.0f / 3.0f, 0.1f, 100.0f);
   glm::mat4 View = glm::lookAt(
-      glm::vec3(4, 3, 3), // Camera is at (4,3,3), in World Space
+      glm::vec3(4, 3, -3), // Camera is at (4,3,3), in World Space
       glm::vec3(0, 0, 0), // and looks at the origin
       glm::vec3(0, 1, 0)  // Head is up (set to 0,-1,0 to look upside-down)
   );
-  glm::mat4 Model = glm::mat4(1.0f);
+
+  auto Model = glm::mat4(1.0f);
   glm::mat4 MVP = Projection * View * Model;
 
   static const GLfloat g_vertex_buffer_data[] = {
-      -1.0f, -1.0f, 0.0f,
-      1.0f, -1.0f, 0.0f,
-      0.0f,  1.0f, 0.0f,
+      -1.0f,-1.0f,-1.0f,
+      -1.0f,-1.0f, 1.0f,
+      -1.0f, 1.0f, 1.0f,
+      1.0f, 1.0f,-1.0f, // triangle 2 : begin
+      -1.0f,-1.0f,-1.0f,
+      -1.0f, 1.0f,-1.0f, // triangle 2 : end
+      1.0f,-1.0f, 1.0f,
+      -1.0f,-1.0f,-1.0f,
+      1.0f,-1.0f,-1.0f,
+      1.0f, 1.0f,-1.0f,
+      1.0f,-1.0f,-1.0f,
+      -1.0f,-1.0f,-1.0f,
+      -1.0f,-1.0f,-1.0f,
+      -1.0f, 1.0f, 1.0f,
+      -1.0f, 1.0f,-1.0f,
+      1.0f,-1.0f, 1.0f,
+      -1.0f,-1.0f, 1.0f,
+      -1.0f,-1.0f,-1.0f,
+      -1.0f, 1.0f, 1.0f,
+      -1.0f,-1.0f, 1.0f,
+      1.0f,-1.0f, 1.0f,
+      1.0f, 1.0f, 1.0f,
+      1.0f,-1.0f,-1.0f,
+      1.0f, 1.0f,-1.0f,
+      1.0f,-1.0f,-1.0f,
+      1.0f, 1.0f, 1.0f,
+      1.0f,-1.0f, 1.0f,
+      1.0f, 1.0f, 1.0f,
+      1.0f, 1.0f,-1.0f,
+      -1.0f, 1.0f,-1.0f,
+      1.0f, 1.0f, 1.0f,
+      -1.0f, 1.0f,-1.0f,
+      -1.0f, 1.0f, 1.0f,
+      1.0f, 1.0f, 1.0f,
+      -1.0f, 1.0f, 1.0f,
+      1.0f,-1.0f, 1.0f
   };
 
-  GLuint vertexbuffer;
-  glGenBuffers(1, &vertexbuffer);
-  glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+  static const GLfloat g_color_buffer_data[] = {
+      0.583f,  0.771f,  0.014f,
+      0.609f,  0.115f,  0.436f,
+      0.327f,  0.483f,  0.844f,
+      0.822f,  0.569f,  0.201f,
+      0.435f,  0.602f,  0.223f,
+      0.310f,  0.747f,  0.185f,
+      0.597f,  0.770f,  0.761f,
+      0.559f,  0.436f,  0.730f,
+      0.359f,  0.583f,  0.152f,
+      0.483f,  0.596f,  0.789f,
+      0.559f,  0.861f,  0.639f,
+      0.195f,  0.548f,  0.859f,
+      0.014f,  0.184f,  0.576f,
+      0.771f,  0.328f,  0.970f,
+      0.406f,  0.615f,  0.116f,
+      0.676f,  0.977f,  0.133f,
+      0.971f,  0.572f,  0.833f,
+      0.140f,  0.616f,  0.489f,
+      0.997f,  0.513f,  0.064f,
+      0.945f,  0.719f,  0.592f,
+      0.543f,  0.021f,  0.978f,
+      0.279f,  0.317f,  0.505f,
+      0.167f,  0.620f,  0.077f,
+      0.347f,  0.857f,  0.137f,
+      0.055f,  0.953f,  0.042f,
+      0.714f,  0.505f,  0.345f,
+      0.783f,  0.290f,  0.734f,
+      0.722f,  0.645f,  0.174f,
+      0.302f,  0.455f,  0.848f,
+      0.225f,  0.587f,  0.040f,
+      0.517f,  0.713f,  0.338f,
+      0.053f,  0.959f,  0.120f,
+      0.393f,  0.621f,  0.362f,
+      0.673f,  0.211f,  0.457f,
+      0.820f,  0.883f,  0.371f,
+      0.982f,  0.099f,  0.879f
+  };
+
+  GLuint vertexBuffer;
+  glGenBuffers(1, &vertexBuffer);
+  glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
   glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
 
+  GLuint colorBuffer;
+  glGenBuffers(1, &colorBuffer);
+  glBindBuffer(GL_ARRAY_BUFFER, colorBuffer);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
   do {
-    glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     glUseProgram(programID);
     glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
 
     glEnableVertexAttribArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
     glVertexAttribPointer(
         0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
         3,                  // size
@@ -99,7 +180,18 @@ int main() {
         (void *) nullptr    // array buffer offset
     );
 
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glEnableVertexAttribArray(1);
+    glBindBuffer(GL_ARRAY_BUFFER, colorBuffer);
+    glVertexAttribPointer(
+        1,                  // attribute 1. No particular reason for 1, but must match the layout in the shader.
+        3,                  // size
+        GL_FLOAT,           // type
+        GL_FALSE,       // normalized?
+        0,                  // stride
+        (void *) nullptr    // array buffer offset
+    );
+
+    glDrawArrays(GL_TRIANGLES, 0, 3*12);
 
     glDisableVertexAttribArray(0);
 
@@ -112,7 +204,8 @@ int main() {
       glfwWindowShouldClose(window) == 0);
 
   // Cleanup VBO
-  glDeleteBuffers(1, &vertexbuffer);
+  glDeleteBuffers(1, &vertexBuffer);
+    glDeleteBuffers(1, &colorBuffer);
   glDeleteVertexArrays(1, &VertexArrayID);
   glDeleteProgram(programID);
 
